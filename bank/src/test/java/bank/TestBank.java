ff61750acdcc601f5ecb40520d601eb25810e9d0
LazeJL <67427143+SPycre@users.noreply.github.com>
2023-03-28 15:15:56 +0200

    add BANK

---
+++ bank/src/test/java/bank/TestBank.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package bank;
+
+import bank.Account;
+import bank.Bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestBank {
+
+	@Test
+	public void testBank() {
+
+		Account account1 = new Account(1, 100, 0.1f);
+		Account account2 = new Account(2, 200, 0.1f);
+		Bank bank = new Bank(new ArrayList<>(List.of(account1, account2)));
+		Account account3 = new Account(3, 300, 0.1f);
+		bank.addAccount(account3);
+		bank.transfer(account1, account2, 50);
+		bank.showBalance();
+		assertEquals(50, account1.getBalance());
+	}
+}
